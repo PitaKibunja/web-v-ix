447e321da743062d01b4373c42b5b907a64bacb3
MAIN-ACC <pitakibunja@gmail.com>
2021-09-28 10:08:51 +0300

    to address the issue

--- src/components/navs/LinkShortcut.vue
+++ src/components/navs/LinkShortcut.vue
@@ -24,102 +24,10 @@
          
     </div>
     </v-row>
-   
-   
-
-    <!-- <v-btn
-      class="mx-2"
-      fab
-      dark
-      small
-      color="pink"
-    >
-      <v-icon dark>
-        mdi-heart
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="indigo"
-    >
-      <v-icon dark>
-        mdi-plus
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="teal"
-    >
-      <v-icon dark>
-        mdi-format-list-bulleted-square
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      large
-      color="cyan"
-    >
-      <v-icon dark>
-        mdi-pencil
-      </v-icon>
-    </v-btn>
-
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      large
-      color="purple"
-    >
-      <v-icon dark>
-        mdi-android
-      </v-icon>
-    </v-btn> -->
   </div>
-  
-  
-  <!-- <v-card-text>
-    <v-spaceer></v-spaceer>
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          class="  justify-space-between mt-2 mb-6 mx-16 primary--text"
-          color="primary"
-          icon
-          
-        >
-        <div class="ml-10 pb-8 pt-8">
-          <v-btn
-            elevation="2"
-            fab
-            color="primary"
-            small
-          >
-                    <v-icon size="35px">
-            {{ link.icon }}
-          </v-icon>
-          </v-btn>
-
-         <div class="mx-14 pt-3">
-               {{ link.text }}
-         </div>
-    
-        
-          </div>
-        </v-btn>
-      </v-card-text> -->
 </template>
 <script>
-// import UtilityLinks from './UtilityLinks.vue'
+
   export default {
     data () {
       return {
