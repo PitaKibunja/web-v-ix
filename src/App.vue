f991cc7f9fec9c8598dd7fb6a0ca6c3429818781
MAIN-ACC <pitakibunja@gmail.com>
2021-10-04 17:05:58 +0300

    finish navigation once and for all

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,6 @@
 <template>
   <v-app>
-<v-toolbar app color="primary" flat   class="mt-n1">
-  <v-card  width="100" class="my-1 mx-1 pt-0" z-index="200">
-           <v-img contains transition="scale-transition" width="100" src="./assets/tveta_logo.png" clas="mt-n16 pa-1 shrink"></v-img>      
-  </v-card>
-      <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="sidebar = !sidebar">
-        </v-toolbar-side-icon>
-      </span>
-      <v-toolbar-title >
-        <router-link to="/" tag="span" style="cursor: pointer">
-          
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only pa-2" >
-        <v-btn
-        color="primary"
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-        
-      </v-toolbar-items>
-     <v-spacer></v-spacer>
-      <v-card  color="primary" class="mt-n1" flat>
-          <v-card-title>
-            <v-btn><v-icon>mdi-magnify</v-icon></v-btn>
-        </v-card-title>
-        </v-card>
-              <v-card  color="primary" class="mt-2" flat>
-          <v-card-title>
-            <v-btn><v-icon>mdi-chevron</v-icon></v-btn>
-        </v-card-title>
-        </v-card>
-    </v-toolbar>
+    <TopNav/>
     <v-main style="padding:0px; background: white ">
       <router-view/>
       <Assistant/>
@@ -49,13 +12,13 @@
 <script>
 import Footer from './components/Footer.vue'
 // import TopBar from './components/TopBar.vue'
-// import TopNav from './components/TopNav.vue'
+import TopNav from './components/TopNav.vue'
 import Assistant from './components/Assistant.vue'
 export default {
   name: 'App',
   components:{
     Footer,
-    // TopNav,
+    TopNav,
     Assistant
   },
 
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,72 +1,95 @@
 <template>
- <div class="mr-4">
-    <v-toolbar app color="primary">
-      <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="sidebar = !sidebar">
-        </v-toolbar-side-icon>
-      </span>
-      <v-toolbar-title color="primary">
-        <router-link to="/" tag="span" style="cursor: pointer">
-          {{ appTitle }}
-        </router-link>
-      </v-toolbar-title>
+  <div>
+    <v-app-bar
+      color="primary accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>TVET Authority</v-toolbar-title>
+
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only pa-2" >
+      <template>
+  <div class="text-center">
+    <v-menu
+      open-on-hover
+      top
+      offset-y
+      tile
+    >
+      <template v-slot:activator="{ on, attrs }">
         <v-btn
-        color="primary"
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Dropdown
         </v-btn>
-        
-      </v-toolbar-items>
-     
-      <v-card>
-          <v-card-title>
-      
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        dense
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-        </v-card>
-    </v-toolbar>
- </div>
-    
+      </template>
 
-
+      <v-list>
+          <v-list-item
+            v-for="n in quick"
+            :key="n"
+            @click="() => {}"
+            :to="n.link"
+          >
+            <v-list-item-title>{{ n.Staff }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-menu>
+  </div>
 </template>
 
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in quick"
+            :key="n"
+            @click="() => {}"
+            :to="n.link"
+          >
+            <v-list-item-title>{{ n.Staff }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+</template>
 <script>
-//import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-  data(){
-    return {
-      appTitle: 'TVET Authority',
-      sidebar: false,
-      menuItems: [
-          { title: 'Home', path: '/', icon: 'mdi-home' },
-          { title: 'About us', path: '/signup', icon: 'mdi-face' },
-          { title: 'Services', path: '/signin', icon: 'mdi-lock_open' },
-          { title: 'Media Center', path: '/signin', icon: 'mdi-lock_open' },
-          { title: 'Institutions', path: '/signin', icon: 'mdi-lock_open' },
-          { title: 'Trainers', path: '/signin', icon: 'mdi-lock_open' },
-          { title: 'Curricula', path: '/signin', icon: 'mdi-lock_open' },
-          { title: 'Downloads', path: '/downloads', icon: 'mdi-lock_open' }
-     ]
-    }
-  },
-};
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+      quick:[
+        {Staff:'Staff mail',link:'/'},
+        {Staff:'Staff Portal',link:'/institution'},
+        {Staff:'Staff Portal',link:'/downloads'},
+        {Staff:'Staff Portal',link:'/institution'},
+      ]
+    }),
+  }
 </script>
-<style>
-
-</style>
