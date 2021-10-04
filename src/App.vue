4bacba70ebd0c2bfe3c0dc8416a9273bd3255735
MAIN-ACC <pitakibunja@gmail.com>
2021-10-04 09:34:25 +0300

    to work on dropdown menus from now

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-<v-toolbar app color="primary" flat>
-  <v-card class=" pa-2">
-           <v-img width="100" src="./assets/tveta_logo.png" clas="mt-n16"></v-img>      
+<v-toolbar app color="primary" flat   class="mt-n1">
+  <v-card  width="100" class="my-1 mx-1 pt-0" z-index="200">
+           <v-img contains transition="scale-transition" width="100" src="./assets/tveta_logo.png" clas="mt-n16 pa-1 shrink"></v-img>      
   </v-card>
       <span class="hidden-sm-and-up">
         <v-toolbar-side-icon @click="sidebar = !sidebar">
@@ -26,19 +26,16 @@
         </v-btn>
         
       </v-toolbar-items>
-     
-      <v-card elevation="2">
+     <v-spacer></v-spacer>
+      <v-card  color="primary" class="mt-n1" flat>
           <v-card-title>
-      
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        dense
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
+            <v-btn><v-icon>mdi-magnify</v-icon></v-btn>
+        </v-card-title>
+        </v-card>
+              <v-card  color="primary" class="mt-2" flat>
+          <v-card-title>
+            <v-btn><v-icon>mdi-chevron</v-icon></v-btn>
+        </v-card-title>
         </v-card>
     </v-toolbar>
     <v-main style="padding:0px; background: white ">
