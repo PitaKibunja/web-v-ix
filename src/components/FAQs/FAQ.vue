602d6a27a993afed5aaf9ab69ba530e5a7417fef
PitaKibunja <pitakibunja@gmail.com>
2021-10-07 06:07:54 +0300

    faq done now on particular faq

--- src/components/FAQs/FAQ.vue
+++ src/components/FAQs/FAQ.vue
@@ -12,7 +12,7 @@
             <v-divider></v-divider>
             <v-card>
                 <v-card-title>
-                 <v-row cols="12 " class="d-flex justify-center mb-1 ml-4  pt-2">
+                 <v-row cols="8" class="d-flex justify-center mb-1 ml-4  pt-2">
                     <div class="font-weight-medium">
                 
                  <h3>How can we assist you today?</h3>  <br/>    
@@ -33,15 +33,30 @@
                     </v-row>
             </v-card-title>
             </v-card>
-            <v-card >
+            <v-container fluid>
+              <v-card class="d-flex align-content-start flex-wrap ma-2 pt-2" elevation="3">
                 <v-row
       no-gutters
-      style="flex-wrap: nowrap;"
+      
     >
-    <v-container>
-         <v-row align="center">
+
+          <v-btn 
+        class="ma-4 pa-8 ma-2 text-white font-weight-medium"
+        large
+        
+        v-for="n in faqs"
+        max-width="400"
+       
+        :key="n"
+        :to="n.link"
+        >
+            {{ n.name }} <v-icon>{{ n.icon }}</v-icon>
+        </v-btn>
+    
+        
+         <!-- <v-row align="center">
              <v-col
-        cols="4"
+        cols="12"
        
         class="flex-grow-0 flex-shrink-0 pa-2 ma-2 pt-n8"
       >
@@ -106,11 +121,64 @@
         </v-btn>
           
       </v-col>
-         </v-row>
-    </v-container>
+         </v-row> -->
       
     </v-row>
             </v-card>
+            </v-container>
           </v-card>
     </v-container>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        faqs: [
+          {
+            name: 'MIS Institution Registration',
+            icon:'mdi-help',
+            link:'/questions'
+          },{
+            name: 'MIS Institution Registration',
+            icon:'mdi-help',
+            link:'/questions'
+          },{
+            name: 'MIS Institution Registration ',
+            icon:'mdi-help',
+            link:'/questions'
+          },{
+            name: 'MIS Institution Registration ',
+            icon:'mdi-help',
+            link:'/questions'
+          },
+          {
+            name: 'Trainer Registration Process',
+            icon:'mdi-help',
+            link:'/questions'
+          },
+          {
+            name: 'MIS Letter of No Objection',
+            icon:'mdi-help',
+            link:'/questions'
+          },
+            {
+            name: 'TVET Standards and Guidelines',
+            icon:'mdi-help',
+            link:'/questions'
+          },
+            {
+            name: 'Access to Outreach Services',
+            icon:'mdi-help',
+            link:'/questions'
+          },
+            {
+            name: 'TVET Trainers Employment',
+            icon:'mdi-help',
+            link:'/questions'
+          },
+
+        ],
+      }
+    },
+  }
+</script>
