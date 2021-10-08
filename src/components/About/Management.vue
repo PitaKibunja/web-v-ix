947cf5ff78197e61457c5ec8b4d53f2c2ef42c50
PitaKibunja <pitakibunja@gmail.com>
2021-10-08 05:30:41 +0300

    work on the rest

--- src/components/About/Management.vue
+++ src/components/About/Management.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="mt-8">
-          <v-card elevation="2">
+          <v-card elevation="2" class="mb-2">
                
             <v-card-title>
                  <v-row cols="12 " class="d-flex justify-center mb-1 ml-4  pt-2">
@@ -20,28 +20,30 @@
                     </v-breadcrumbs>
             </div>
                    <v-divider></v-divider>
-                   <v-card>
-                                   <v-card-title>
-                 <v-row cols="12 " class="d-flex justify-start mb-1 ml-4  pt-2">
+                   <v-container fluid>
+                      <v-card>
+                                    <v-card-title>
+                 <v-row cols="12 " class="d-flex justify-center mb-1 ml-4  pt-2">
                     <div class="font-weight-medium">
                 
-                 <h4>Board Members</h4>    
-                    <v-divider></v-divider>
-                                <v-row>
-                <v-container>
-                    <v-card style="margin: 0px; padding:0px; background: white;  width: 100%">
-                        <div class="ma-2 pa-2">
-                       <v-card
-                        class="d-flex justify-space-between mb-6"
+                 <h5>Board Members</h5>      
+                    </div>
+                    </v-row>
+            </v-card-title>
+            <v-divider></v-divider>
+                      </v-card>
+                     <v-card
+                        class="d-flex justify-space-between mb-6 pt-4"
                         :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
                         flat
                         tile
                         >
+                        <v-row cols="12 " class="d-flex justify-center mb-1  pt-2">
                         <v-card
-                            v-for="n in 3"
+                            v-for="n in 8"
                             :key="n"
-                            class="pa-1"
-                            elevation="2"
+                            class="pa-1 ma-1"
+                            elevation="4"
                             tile
                         >
                             <div class="text-center">
@@ -51,12 +53,14 @@
     >
       <template v-slot:activator="{ on, attrs }">
         <v-card
-          dark
+          
           v-bind="attrs"
           v-on="on"
-          class="ma-4 pa-4"
+          class="ma-4 pa-3"
         >
           <v-img src="../../assets/board.png" width="200"></v-img>
+          <v-card-text class="pt-1 mb-0">John Doe</v-card-text>
+          <v-card-text>Position</v-card-text>
         </v-card>
       </template>
 
@@ -100,17 +104,96 @@
     </v-dialog>
   </div>
                         </v-card>
+                        </v-row>
                         </v-card>
-
-                </div>
-                    </v-card>
-                </v-container>
-            </v-row>
+                   </v-container>
+                    <v-container fluid>
+                      <v-card>
+                                    <v-card-title>
+                 <v-row cols="12 " class="d-flex justify-center mb-1 ml-4  pt-2">
+                    <div class="font-weight-medium">
+                
+                 <h5>Management Team</h5>      
                     </div>
                     </v-row>
             </v-card-title>
-                   </v-card>
-            
+            <v-divider></v-divider>
+                      </v-card>
+                     <v-card
+                        class="d-flex justify-space-between mb-6 pt-4"
+                        :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
+                        flat
+                        tile
+                        >
+                        <v-row cols="12 " class="d-flex justify-center mb-1  pt-2">
+                        <v-card
+                            v-for="n in 8"
+                            :key="n"
+                            class="pa-1 ma-1"
+                            elevation="2"
+                            tile
+                        >
+                            <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-card
+          
+          v-bind="attrs"
+          v-on="on"
+          class="ma-4 pa-4"
+        >
+          <v-img src="../../assets/board.png" width="200"></v-img>
+          <v-card-text class="pt-1 mb-0">John Doe</v-card-text>
+          <v-card-text>Position</v-card-text>
+        </v-card>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-4">
+            <div class="d-flex justify-end">
+                John Doe
+            </div>
+          
+        </v-card-title>
+<v-card-actions class="justify-end">
+              <v-btn
+                text
+                class="pt-n4"
+                @click="dialog.value = false"
+              > <v-icon >mdi-close</v-icon></v-btn>
+            </v-card-actions>
+                     <v-divider
+            class="mx-4"
+            vertical
+            ></v-divider>
+        <v-card-text>
+          <v-img src="../../assets/board.png" width="200"></v-img>
+   
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae, optio unde velit libero repudiandae nesciunt, delectus ex iure odit similique beatae, quia vel debitis in aliquam quasi sit veniam.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+           <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+                        </v-card>
+                        </v-row>
+                        </v-card>
+                   </v-container>
 
           </v-card>
     </v-container>
